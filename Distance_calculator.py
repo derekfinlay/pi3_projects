052eec6283d326bafb8cea2fe5f6c12276049308
derekfinlay <derekmfinlay@gmail.com>
2017-01-14 19:58:37 -0500

    Main project is to create a device to calcluate disstance and display on LCD display.

---
+++ Distance_calculator.py
@@ -0,0 +1,7 @@
+#
+
+# Using the ultrasonic range sensor calculate distance and display on lcd
+# display. Use RGB led to change to red for close and white for distanne
+# Add joystick to toggle display of distance feet/m and temperature in cel/f
+
+
